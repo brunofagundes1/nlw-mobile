73b9704cfc1260e6610276464ff3a8843a0dd220
Bruno <brunopaulsenfagundes@gmail.com>
2023-05-31 13:45:48 -0300

    Aula 5 parte 2

--- app/new.tsx
+++ app/new.tsx
@@ -88,11 +88,15 @@ export default function NewMemory() {
       <View className="mt-4 flex-row items-center justify-between">
         <NLWLogo />
 
-        <Link href="/memories" asChild>
-          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
-            <Icon name="arrow-left" size={16} color="#FFF" />
-          </TouchableOpacity>
-        </Link>
+        <View className="flex-row gap-2">
+          <Link href="/memories" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+              <Icon name="arrow-left" size={16} color="#FFF" />
+            </TouchableOpacity>
+          </Link>
+
+          <View className="invisible h-10 w-10"></View>
+        </View>
       </View>
 
       <View className="mt-6 space-y-6">
@@ -114,6 +118,7 @@ export default function NewMemory() {
           className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
           {preview ? (
+            // eslint-disable-next-line jsx-a11y/alt-text
             <Image
               source={{ uri: preview }}
               className="h-full w-full rounded-lg object-cover"
@@ -143,7 +148,7 @@ export default function NewMemory() {
           onPress={handleCreateMemory}
           className="items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
-          <Text className="text-bla font-alt text-sm uppercase">Salvar</Text>
+          <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
